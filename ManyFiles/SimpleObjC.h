405405375c9e44aaa6c92775096985e12594cc79
Gregor Dschung <gregor@chkpnt.de>
2018-01-21 12:26:42 +0100

    Add an ObjC-class and bridging header

---
+++ ManyFiles/SimpleObjC.h
@@ -0,0 +1,20 @@
+//
+//  SimpleObjC.h
+//  ManyFiles
+//
+//  Created by Gregor Dschung on 21.01.18.
+//  Copyright © 2018 Gregor Dschung. All rights reserved.
+//
+
+#ifndef SimpleObjC_h
+#define SimpleObjC_h
+
+#import <Foundation/Foundation.h>
+
+@interface SimpleObjC : NSObject
+
+- (void)sayHello;
+
+@end
+
+#endif /* SimpleObjC_h */
---
+++ ManyFiles/SimpleObjC.m
@@ -0,0 +1,17 @@
+//
+//  SimpleObjC.m
+//  ManyFiles
+//
+//  Created by Gregor Dschung on 21.01.18.
+//  Copyright © 2018 Gregor Dschung. All rights reserved.
+//
+
+#import "SimpleObjC.h"
+
+@implementation SimpleObjC
+    
+- (void)sayHello {
+    NSLog(@"Hello, World!");
+}
+    
+@end
